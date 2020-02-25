8b072f2b022c5cf83a8b828174bbfaf6fdb90ea2
Asbarn <47718251+Asbarn@users.noreply.github.com>
2020-02-25 14:44:26 +0300

    Add files via upload

--- TMS_TODOLIST/scripts/index.js
+++ TMS_TODOLIST/scripts/index.js
@@ -1,12 +1,7 @@
-// $(document).ready(function() {
-//     var FirstTab = document.querySelector('#FirstTab');
-//     for (var i = 0; i < data.length; i++) {
-//         FirstTab.innerHTML += `<div class="row task"><h5>${data[i].firstName}</h5> <h7> ${data[i].lastName} </h7> <button> Редактировать</button><button> Выполнено</button><button> Удалить</button>  </div>`
-//     }
-// });
 var fish = [{
         name: "Wash dishes",
-        description: "none"
+        description: "none",
+        priority: 1,
     },
 
 
@@ -15,17 +10,21 @@ var fish = [{
 
 var fish2 = [{
     name: "Go to the store",
-    description: "none"
+    description: "none",
+    priority: 2,
 }, ];
 
 var fish3 = [{
         name: "Go to study",
-        description: "none"
+        description: "none",
+        priority: 3,
     },
 
 ];
 
-
+function mySort(field) {
+    return (a, b) => a[field] < b[field] ? 1 : -1;
+}
 
 
 var table = document.querySelector('#FirstTab');
@@ -152,8 +151,8 @@ document.getElementById("defaultOpenTest").click();
 
 
 
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
+var modal = document.querySelector("#myModal");
+var btn = document.querySelector("#myBtn");
 var span = document.getElementsByClassName("cancel")[0];
 btn.onclick = function() {
     modal.style.display = "block";
@@ -196,6 +195,7 @@ $("body").on('click', '.changeButton', function() {
             }
             text1.value = newPerson.name;
             text2.value = newPerson.description;
+
             changingPersonIndex = i;
 
         }
@@ -223,39 +223,73 @@ let changeHuman = document.querySelector('#changePerson');
 changeHuman.addEventListener('click', function changePerson() {
     var text1 = document.querySelector("#firstNameChange");
     var text2 = document.querySelector("#lastNameChange");
-    console.log(text1);
-    let newPerson = {
-        name: `${text1.value}`,
-        description: `${text2.value}`
-    };
-    console.log(newPerson);
-    var tabs = document.getElementsByClassName("row");
+    var prior;
+    switch (document.querySelector("#priorChange").value) {
+        case "Бессрочное":
+            prior = 0;
+            break;
+        case "Триальное":
+            prior = 1;
+            break;
+        case "Срочное":
+            prior = 2;
+            break;
+        default:
+            prior = 0;
+            break;
 
-    for (let i = 0; i < tabs.length; i++) {
 
-
-
-
-        if (changingPersonIndex == i && i < activeData.length) {
-
-            tabs[i].innerHTML = `<h5><p>${newPerson.name}</p></h5> <h7> <p>${newPerson.description}</p> </h7> <button class="changeButton"> Редактировать</button><button class="doButton"> Выполнено</button><button class="deleteButton"> Удалить</button> `;
-
-            activeData[i].name = newPerson.name;
-            activeData[i].description = newPerson.description;
-
-            localStorage.setItem("ActiveTable", JSON.stringify(activeData));
-
-        } else if (changingPersonIndex == i && i >= activeData.length) {
-            tabs[i].innerHTML = `<h5><p>${newPerson.name}</p></h5> <h7> <p>${newPerson.description}</p> </h7> <button class="changeButton"> Редактировать</button> <button class="deleteButton"> Удалить</button> `;
-            doneData[i - activeData.length] = newPerson;
-            doneData[i - activeData.length].name = newPerson.name;
-            doneData[i - activeData.length].description = newPerson.description;
-            localStorage.setItem("DoneTable", JSON.stringify(doneData));
-
-        }
 
     }
-    modalChanges.style.display = "none";
+    let newPerson = {
+        name: `${text1.value}`,
+        description: `${text2.value}`,
+        priority: prior,
+    };
+    var tabs = document.getElementsByClassName("row");
+    if (newPerson.name.length == 0) {
+        text1.classList.add('error');
+        var taskName = document.querySelector(".taskNameChange");
+        taskName.innerText = ("Задание" + "(Это поле обязательно для заполнения)!");
+        taskName.classList.add('error');
+    } else {
+        text1.classList.remove('error');
+        document.querySelector(".taskNameChange").classList.remove('error');
+        document.querySelector(".taskNameChange").innerText = "Задание";
+        for (let i = 0; i < tabs.length; i++) {
+
+
+
+
+            if (changingPersonIndex == i && i < activeData.length) {
+
+                tabs[i].innerHTML = `<h5><p>${newPerson.name}</p></h5> <h7> <p>${newPerson.description}</p> </h7> <button class="changeButton"> Редактировать</button><button class="doButton"> Выполнено</button><button class="deleteButton"> Удалить</button> `;
+                activeData[i].name = newPerson.name;
+                activeData[i].description = newPerson.description;
+                console.log(activeData[i].priority, newPerson.priority);
+
+                activeData[i].priority = newPerson.priority;
+                activeData.sort(mySort('priority'));
+                table.innerHTML = "";
+                for (var k = 0; k < activeData.length; k++) {
+                    table.innerHTML += `<div class="row task"><h5><p>${activeData[k].name}</p></h5> <h7> <p>${activeData[k].description}</p> </h7> <button class="changeButton"> Редактировать</button><button class="doButton"> Выполнено</button><button class="deleteButton"> Удалить</button>  </div>`
+
+                }
+                localStorage.setItem("ActiveTable", JSON.stringify(activeData));
+                break;
+            } else if (changingPersonIndex == i && i >= activeData.length) {
+                console.log(2);
+                tabs[i].innerHTML = `<h5><p>${newPerson.name}</p></h5> <h7> <p>${newPerson.description}</p> </h7> <button class="changeButton"> Редактировать</button> <button class="deleteButton"> Удалить</button> `;
+                doneData[i - activeData.length] = newPerson;
+                doneData[i - activeData.length].name = newPerson.name;
+                doneData[i - activeData.length].description = newPerson.description;
+                localStorage.setItem("DoneTable", JSON.stringify(doneData));
+                break;
+            }
+
+        }
+        modalChanges.style.display = "none";
+    }
 })
 
 
@@ -265,24 +299,54 @@ addHuman.addEventListener('click', function addPerson() {
 
     var text1 = document.getElementById("firstNameAdd");
     var text2 = document.getElementById("lastNameAdd");
+    var prior;
+    switch (document.querySelector("#prior").value) {
+        case "Бессрочное":
+            prior = 0;
+            break;
+        case "Триальное":
+            prior = 1;
+            break;
+        case "Срочное":
+            prior = 2;
+            break;
+        default:
+            prior = 0;
+            break;
 
+
+
+    }
     let newPerson = {
         name: text1.value,
         description: text2.value,
-
+        priority: prior,
     }
 
 
-    table.innerHTML += `<div class="row task"><h5><p>${newPerson.name}</p></h5> <h7> <p>${newPerson.description}</p> </h7> <button class="changeButton"> Редактировать</button><button class="doButton"> Выполнено</button><button class="deleteButton"> Удалить</button>  </div>`
-    activeData.push(newPerson);
-    console.log(activeData);
 
-    document.getElementById("firstNameAdd").value = "";
-    document.getElementById("lastNameAdd").value = "";
-    modal.style.display = "none";
-    localStorage.setItem("ActiveTable", JSON.stringify(activeData));
-    console.log(localStorage.getItem("ActiveTable"));
+    if (newPerson.name.length == 0) {
+        text1.classList.add('error');
+        var taskName = document.querySelector(".taskName");
+        taskName.innerText = ("Задание" + "(Это поле обязательно для заполнения)!");
+        taskName.classList.add('error');
+    } else {
+        text1.classList.remove('error');
+        document.querySelector(".taskName").classList.remove('error');
+        document.querySelector(".taskName").innerText = "Задание";
+        table.innerHTML += `<div class="row task"><h5><p>${newPerson.name}</p></h5> <h7> <p>${newPerson.description}</p> </h7> <button class="changeButton"> Редактировать</button><button class="doButton"> Выполнено</button><button class="deleteButton"> Удалить</button>  </div>`
+        activeData.push(newPerson);
+        activeData.sort(mySort('priority'));
+        table.innerHTML = "";
+        for (var i = 0; i < activeData.length; i++) {
+            table.innerHTML += `<div class="row task"><h5><p>${activeData[i].name}</p></h5> <h7> <p>${activeData[i].description}</p> </h7> <button class="changeButton"> Редактировать</button><button class="doButton"> Выполнено</button><button class="deleteButton"> Удалить</button>  </div>`
+        }
 
+        document.getElementById("firstNameAdd").value = "";
+        document.getElementById("lastNameAdd").value = "";
+        modal.style.display = "none";
+        localStorage.setItem("ActiveTable", JSON.stringify(activeData));
+    }
 })
 
 $("body").on('click', '.deleteButton', function() {
@@ -327,7 +391,6 @@ $("body").on('click', '.deleteButton', function() {
             deletedData.push(newPerson);
 
             localStorage.setItem("DeletedTable", JSON.stringify(deletedData));
-            //document.location.reload(true); ///????
             break;
         }
 
@@ -387,11 +450,15 @@ $("body").on('click', '.restoreButton', function() {
                 tabs[i + 1].classList.remove('task');
                 tabs[i + 1].classList.remove('row');
                 deletedData.splice(i - (activeData.length + doneData.length), 1);
-                console.log(tabs[i]);
                 localStorage.setItem("DeletedTable", JSON.stringify(deletedData));
 
 
                 activeData.push(newPerson);
+                activeData.sort(mySort('priority'));
+                table.innerHTML = "";
+                for (var k = 0; k < activeData.length; k++) {
+                    table.innerHTML += `<div class="row task"><h5><p>${activeData[k].name}</p></h5> <h7> <p>${activeData[k].description}</p> </h7> <button class="changeButton"> Редактировать</button><button class="doButton"> Выполнено</button><button class="deleteButton"> Удалить</button>  </div>`
+                }
                 localStorage.setItem("ActiveTable", JSON.stringify(activeData));
                 break;
             }
